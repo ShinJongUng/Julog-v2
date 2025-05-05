785e30d33dfbf870f112a3669bbaa2a50bb80eb5
ShinJongUng <whddnd0728@gmail.com>
2025-05-06 02:36:55 +0900

    fix: github 이미지 안불러와지는 에러 pattern 추가

--- next.config.mjs
+++ next.config.mjs
@@ -5,13 +5,18 @@ import rehypeSlug from "rehype-slug";
 const nextConfig = {
   pageExtensions: ["ts", "tsx", "js", "jsx", "md", "mdx"],
   reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "avatars.githubusercontent.com",
+      },
+    ],
+  },
   experimental: {
     mdxRs: {
       remarkPlugins: [remarkGfm],
-      rehypePlugins: [
-        rehypeSlug,
-        // rehype-pretty-code 관련 설정 완전 제거
-      ],
+      rehypePlugins: [rehypeSlug],
     },
   },
 };
